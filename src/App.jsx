2142501cf86420f4697f4bfae9bc26fe3a0b6d0d
Riju Mondal <contact.rijumondal@gmail.com>
2024-02-06 08:53:02 +0530

    now able to fetch Auth cred to mongoDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './Components/Home.jsx';
 import './index.css';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -6,7 +6,36 @@ import ChatApp from './Components/ChatApp.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  const { isLoading, error } = useAuth0();
+  const { user, isLoading, error } = useAuth0();
+
+  useEffect(() => {
+    const uploadUserToMongoDB = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/uploadUser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: user.name,
+            email: user.email,
+          }),
+        });
+
+        if (response.ok) {
+          console.log('User uploaded to MongoDB');
+        } else {
+          console.error('Failed to upload user to MongoDB');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (user) {
+      uploadUserToMongoDB();
+    }
+  }, [user]); 
 
   if (isLoading) {
     return <div>Loading...</div>;
