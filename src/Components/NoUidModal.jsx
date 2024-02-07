07e2b345a022eae17f1d455b23fcc1156c5a3065
Riju Mondal <contact.rijumondal@gmail.com>
2024-02-07 09:27:06 +0530

    added the generate random uid option

--- src/Components/NoUidModal.jsx
+++ src/Components/NoUidModal.jsx
@@ -1,9 +1,28 @@
-// NoUidModal.jsx
 import React, { useState } from 'react';
+import { RiFileCopyLine } from 'react-icons/ri'; // Importing the copy icon
 import '../CompStyles/NoUidModal.css';
 
 const NoUidModal = ({ onRequestClose }) => {
   const [uid, setUid] = useState('');
+
+  // Function to generate a random UID
+  const generateRandomUid = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < 16; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  };
+
+  const handleGenerateUid = () => {
+    const generatedUid = generateRandomUid();
+    setUid(generatedUid);
+  };
+
+  const handleCopyUid = () => {
+    navigator.clipboard.writeText(uid);
+  };
 
   const handleUploadUserWithUid = async () => {
     try {
@@ -32,7 +51,11 @@ const NoUidModal = ({ onRequestClose }) => {
       <div className="modal">
         <h2>No UID Found</h2>
         <p>Please provide a UID to continue.</p>
-        <input type="text" placeholder="Enter UID" value={uid} onChange={(e) => setUid(e.target.value)} />
+        <div className="uid-input-container">
+          <input type="text" placeholder="Enter UID" value={uid} onChange={(e) => setUid(e.target.value)} />
+          <button className="copy-button" onClick={handleCopyUid}><RiFileCopyLine /></button>
+        </div>
+        <button onClick={handleGenerateUid}>Generate Random UID</button>
         <button onClick={handleUploadUserWithUid}>Upload User</button>
         <button onClick={onRequestClose}>Close</button>
       </div>
